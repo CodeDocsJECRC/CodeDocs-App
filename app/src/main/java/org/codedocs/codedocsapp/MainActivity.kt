13bd09a36c3f9f8d6036436821399421f7041b70
abhimanyu121 <abhimanyushekhawat17.as@gmail.com>
2018-07-28 16:00:46 +0530

    fragment transcation optimised

--- app/src/main/java/org/codedocs/codedocsapp/MainActivity.kt
+++ app/src/main/java/org/codedocs/codedocsapp/MainActivity.kt
@@ -1,37 +1,77 @@
 package org.codedocs.codedocsapp
 
+
 import android.content.Intent
 import android.os.Bundle
 import android.support.design.widget.BottomNavigationView
+import android.support.v4.app.Fragment
 import android.support.v7.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+    private var id:String?=null
 
     private val mOnNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
         when (item.itemId) {
 
             R.id.xhome -> {
+                var newFrag: Fragment?=null
                 val fm=supportFragmentManager
                 val ft=fm.beginTransaction()
-                val fragment=home()
-                ft.replace(R.id.container,fragment).addToBackStack(null).commit();
+                val oldfragment=fm.findFragmentByTag(""+id)
+                if(oldfragment!=null)
+                    ft.hide(oldfragment)
+                newFrag=fm.findFragmentByTag("1")
+                if(newFrag==null){
+                    newFrag=home()
+                    ft.add(R.id.container,newFrag,"1")
 
+                }
+
+
+                ft.show( newFrag );
+                ft.commit();
+                id= 1.toString()
                 return@OnNavigationItemSelectedListener true
             }
             R.id.xnotifs -> {
+                var newFrag: Fragment?=null
                 val fm=supportFragmentManager
                 val ft=fm.beginTransaction()
-                val fragment=notifications()
-                ft.replace(R.id.container,fragment).addToBackStack(null).commit();
+                val oldfragment=fm.findFragmentByTag(""+id)
+                if(oldfragment!=null)
+                    ft.hide(oldfragment)
+                newFrag=fm.findFragmentByTag("2")
+                if(newFrag==null){
+                    newFrag=notifications()
+                    ft.add(R.id.container,newFrag,"2")
 
+                }
+
+
+                ft.show( newFrag );
+                ft.commit();
+                id= 2.toString()
                 return@OnNavigationItemSelectedListener true
             }
             R.id.xweird -> {
+                var newFrag: Fragment?=null
                 val fm=supportFragmentManager
                 val ft=fm.beginTransaction()
-                val fragment=calander()
-                ft.replace(R.id.container,fragment).addToBackStack(null).commit();
+                val oldfragment=fm.findFragmentByTag(""+id)
+                if(oldfragment!=null)
+                    ft.hide(oldfragment)
+                newFrag=fm.findFragmentByTag("3")
+                if(newFrag==null){
+                    newFrag=calander()
+                    ft.add(R.id.container,newFrag,"3")
+
+                }
+
+
+                ft.show( newFrag );
+                ft.commit();
+                id= 3.toString()
                 return@OnNavigationItemSelectedListener true
             }
         }
@@ -44,7 +84,9 @@ class MainActivity : AppCompatActivity() {
         val fm=supportFragmentManager
         val ft=fm.beginTransaction()
         val fragment=home()
-        ft.replace(R.id.container,fragment).addToBackStack(null).commit();
+        id= 1.toString()
+        ft.replace(R.id.container,fragment,id).addToBackStack(null).commit();
+        id= 1.toString()
         val intent = Intent(this,fcm::class.java)
         startService(intent)
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener)
